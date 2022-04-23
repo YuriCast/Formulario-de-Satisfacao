103db77c1c35d40da17f2b4563f09b80519c1318
Yuri Castilho <99267254+YuriCast@users.noreply.github.com>
2022-04-23 16:29:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,18 +8,16 @@ function enviarResposta(){
     let q1_3 = document.querySelector('#q1_3')
     let q1_4 = document.querySelector('#q1_4')
     let q1_5 = document.querySelector('#q1_5')
-}
-
-if (q1_1.checked){
-
-} else if (q1_2.checked){
-    alert('Obrigado pela boa avaliação')
-} else if (q1_3.checked){
-    alert('Obrigado pela boa avaliação')
-} else if (q1_4.checked){
-    alert('Obrigado pela boa avaliação')
-} else if (q1_5.checked){
-    alert('Obrigado pela boa avaliação')
-} else {
-    alert('Obrigado pela boa avaliação')
+   
+    if (q1_1.checked){
+        alert('Muito obrigado pela boa avaliação')
+    } else  if (q1_2.checked){
+        alert('Obrigado pela boa avaliação')
+    } else  if (q1_3.checked){
+        alert('Obrigado')
+    } else  if (q1_4.checked){
+        alert('Tentaremos melhorar')
+    } else  if (q1_5.checked){
+        alert('Faremos o possível para melhorarmos')
+    }
 }
